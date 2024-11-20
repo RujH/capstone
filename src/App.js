5c2ad745cc83632c81183b34617600ef4a688b93
rujh <rujhaan01@gmail.com>
2024-11-19 21:34:03 -0600

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import LandingPage from './components/landingPage';
-import Header from './components/header'; 
 import Services from './components/services';
 import AboutUs from './components/aboutUs';
 import ContactUs from './components/contactUs';
